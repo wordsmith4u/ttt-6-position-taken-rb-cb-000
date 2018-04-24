ea4a42f74353cbab8ca2a7f6e8c5985a20e891a7
Ronald Gerlach <wordsmith4u@hotmail.com>
2018-04-24 14:38:19 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,17 +1,8 @@
 # code your #position_taken? method here!
-ttt_board = ["",""," "," ","X"," "," "," ",""]
-
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
-  elsif board[index] == "X" || board[index] == "O"
+  else
     return true
   end
-
 end
-
-puts position_taken?(ttt_board, 0)
-puts position_taken?(ttt_board, 1)
-puts position_taken?(ttt_board, 2)
-puts position_taken?(ttt_board, 3)
-puts position_taken?(ttt_board, 4)
